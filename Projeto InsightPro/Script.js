3d6e5e97a328e0a647e9356e02abf9deca99024c
Alefemoura <alefecamposdemoura@gmail.com>
2024-06-18 22:21:18 -0300

    Add files via upload

---
+++ Projeto InsightPro/Script.js
@@ -0,0 +1,4 @@
+document.querySelector('.login-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Previne o recarregamento da página
+    alert('Login realizado com sucesso!');
+});
